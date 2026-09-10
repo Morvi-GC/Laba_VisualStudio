081cb6f1d9533fa72ae2e6f8b28dbe50603af2e5
Morvi-GC <morvi1501@gmail.com>
2026-09-10 19:17:23 +0200

    Test_v3
    zaczynamy inicializowanie

--- Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
+++ Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
@@ -3,11 +3,18 @@
 
 using namespace std;
 
+void Initialize();
+
 int main()
 {
    cout << "Test dla gita" << endl;
    cout << "Test 2" << endl;
    cout << "Dodamy jeszcze ten tekst" << endl;
+   Initialize();
 
     return 0;
+}
+void Initialize()
+{ 
+    cout << "Inicializacja gry - 1 etap gameloop" << endl;
 }
