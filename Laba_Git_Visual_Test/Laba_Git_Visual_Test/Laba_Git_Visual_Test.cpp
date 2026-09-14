164440170b2f75d23c0b0b9c6b1685845b086641
Morvi-GC <morvi1501@gmail.com>
2026-09-15 00:13:20 +0200

    początek LZesp v1

--- Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
+++ Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
@@ -1,20 +1,15 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
-void Initialize();
+
 
 int main()
 {
-   cout << "Test dla gita" << endl;
-   cout << "Test 2" << endl;
-   cout << "Dodamy jeszcze ten tekst" << endl;
-   Initialize();
 
-    return 0;
-}
-void Initialize()
-{ 
-    cout << "Inicializacja gry - 1 etap gameloop" << endl;
+
+
+
+
+	return 0;
 }
