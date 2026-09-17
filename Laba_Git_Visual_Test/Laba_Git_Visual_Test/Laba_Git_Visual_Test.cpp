5ec9c3c7bc02819de26f114c126fb8a4c4534cb3
Morvi-GC <morvi1501@gmail.com>
2026-09-17 21:32:02 +0200

    etap7

--- Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
+++ Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
@@ -40,19 +40,32 @@ class Complex_Number //klasa liczba zespolona
 
 		return Result_Sub;
 	}
-	Complex_Number operator* (const Complex_Number& Right_Multi) const // operator mnożenia 
+	Complex_Number operator* (const Complex_Number& Right_Multi) const // operator mnożenia | na krzyż 
 	{
-		double Multi_Real = Real_Number * Right_Multi.Real_Number;
-		double Multi_Imaginary = Imaginary_Number * Right_Multi.Imaginary_Number;
+		double Multi_Real = (Real_Number * Right_Multi.Real_Number) - (Imaginary_Number * Right_Multi.Imaginary_Number);
+		double Multi_Imaginary = (Real_Number * Right_Multi.Imaginary_Number) + (Imaginary_Number * Right_Multi.Real_Number);
 
 		Complex_Number Result_Multi(Multi_Real, Multi_Imaginary);
 
 		return Result_Multi;
 	}
-	Complex_Number operator/ (const Complex_Number& Right_Divide) const // operator dzielenia
+	Complex_Number operator/ (const Complex_Number& Right_Divide) const // operator dzielenia | dzielenie przez wspólny mianownik — sumę kwadratów pól prawej strony
 	{
-		double Divide_Real = Real_Number / Right_Divide.Real_Number;
-		double Divide_Imaginary = Imaginary_Number / Right_Divide.Imaginary_Number;
+		double Denominator = (Right_Divide.Real_Number * Right_Divide.Real_Number ) + (Right_Divide.Imaginary_Number * Right_Divide.Imaginary_Number);
+
+
+			if (Denominator == 0) // wiem że nie można dzielić przez zero 
+		{
+			cout << "Nie mozna dzielic przez zero, wynik zwroce jako 0" << endl;
+			Complex_Number Zero_Result(0, 0);
+
+			return Zero_Result;
+
+		}
+
+		double Divide_Real = ((Real_Number * Right_Divide.Real_Number) + (Imaginary_Number * Right_Divide.Imaginary_Number)) / Denominator;
+		double Divide_Imaginary = ((Right_Divide.Real_Number * Imaginary_Number) - (Real_Number * Right_Divide.Imaginary_Number)) / Denominator;
+			
 
 		Complex_Number Result_Divide(Divide_Real, Divide_Imaginary);
 
@@ -69,12 +82,21 @@ class Complex_Number //klasa liczba zespolona
 int main()
 {
 	Complex_Number LiczbaZesV1(5, -3); // obiekt o klasie Complex_Number z danymi w nawiasach
+	cout << "Nasza podstawowa liczba zespolona to" << endl;
 
 	Complex_Number LiczbaZesV2(LiczbaZesV1); // utworzenie nowego obiektu ze wskazanem kopiowanego obiektu
 	Complex_Number Sum = LiczbaZesV1 + LiczbaZesV2; // wywołanie operatora dodawania dla V1 i V2 oraz utworzenie obiektu Sum z wyniku
 
-	Complex_Number LiczbaZesV3(LiczbaZesV2);
+	Complex_Number LiczbaZesV3(2, 4);
 	Complex_Number Sub = LiczbaZesV2 - LiczbaZesV3;
+
+	Complex_Number LiczbaZesV4(- 6, 4.2);
+	Complex_Number Multi = LiczbaZesV3 * LiczbaZesV4;
+
+	Complex_Number LiczbaZesV5(LiczbaZesV1);
+	Complex_Number Devide = LiczbaZesV3 / LiczbaZesV5;
+	Complex_Number LiczbaZesV6(0, 0);
+	Complex_Number Devide_Zero = LiczbaZesV5 / LiczbaZesV6;
 
 
 
