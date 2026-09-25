ef119f04a187cbedc1b60f338126e5125f42cd0c
Morvi-GC <morvi1501@gmail.com>
2026-09-25 20:34:05 +0200

    Stage_1_Game_Life

--- Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
+++ Laba_Git_Visual_Test/Laba_Git_Visual_Test/Laba_Git_Visual_Test.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-void Render()
-{
+const int BOARD_SIZE = 30;
 
+void Render(int BOARD, int World[][BOARD_SIZE])
+{
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			cout << World[i][j] << "_";
+		}
+		cout << endl;
+	}
 }
 
-const int BOARD_SIZE = 30;
 
 int main()
 {
 	int World[BOARD_SIZE][BOARD_SIZE] = {};
 
+	Render(BOARD_SIZE, World);
+
+	bool Life = 1;
+	bool Dead = 0;
+
 	return 0;
 }
-
-void Render()
-{
-
-};
